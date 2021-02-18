8f30763a6b6bc994201b416b662cc2ad2e0b9104
Brad McGarvey <45945733+EET4340@users.noreply.github.com>
2021-02-18 14:58:34 -0700

    Created firmware project

---
+++ firmware/LED-Controller.X/src/main.c
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////
+// Project: LED-Controller                         //
+// File: main.c                                    //
+// Target: PIC18F27Q43                             // 
+// Compiler: XC8                                   //
+// Author: Brad McGarvey                           //
+// License: GNU General Public License v3.0        //
+// Description: main program                       //
+/////////////////////////////////////////////////////
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
